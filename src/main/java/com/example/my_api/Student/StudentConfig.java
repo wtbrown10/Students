f6fad9417c790ee4b378e021824f32f964c1466a
will_brown10 <wlemogar.brown@nordsonefd.com>
2022-05-16 10:24:48 -0400

    created studentconfig class and added 2 students to database.

---
+++ src/main/java/com/example/my_api/Student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.my_api.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    1L,
+                    "Mariam",
+                    "Mariam.brown@gmail.com",
+                    LocalDate.of(1995, Month.FEBRUARY,10),
+                    25);
+
+            Student alex = new Student(
+                    1L,
+                    "Alex",
+                    "Alex.Smith@gmail.com",
+                    LocalDate.of(1996, Month.MARCH,12),
+                    24);
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
