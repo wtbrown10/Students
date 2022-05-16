405977f4476fa6c331d324d5efb1ffe2f0675777
will_brown10 <wlemogar.brown@nordsonefd.com>
2022-05-16 09:29:53 -0400

    created student repository class

--- src/main/java/com/example/my_api/Student/StudentService.java
+++ src/main/java/com/example/my_api/Student/StudentService.java
@@ -1,21 +1,20 @@
 package com.example.my_api.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public  StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "Mariam.brown@gmail.com",
-                        LocalDate.of(1995, Month.FEBRUARY,10),
-                        25)
-        );
+        return studentRepository.findAll();
     }
 }
